88fbb8dd1c0cb08ffe6dce1c5512bdd91cd57c52
meefr <meefr25@gmail.com>
2025-05-13 19:20:26 +0300

    solve some issues m2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/eventhub-website/',
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Make sure this matches your GitHub Pages setting
+    assetsDir: 'assets'
+  }
 })
